34ab1c995646e6aa3a2d40dcea638180c52d3027
User <user@example.com>
2026-03-24 15:51:12 +0900

    chore: 임시 스크립트 제거

--- fix_data.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = 'd:/코딩/MYMBTI/src/data.js';
-let content = fs.readFileSync(path, 'utf8');
-
-// Replace instances like: "어떤 텍스트 (E)", type: OR "어떤 텍스트 (블라블라)", type:
-content = content.replace(/\s*\([^)]+\)(?=", type:)/g, '');
-
-fs.writeFileSync(path, content, 'utf8');
-console.log('Successfully updated data.js');
